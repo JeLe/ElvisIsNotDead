7272132df27f6b7e1039d3e4e29d4440539d8199
JeLe <dev.lecarpentier@gmail.com>
2014-11-23 19:33:04 +0100

    slider++, header moche mais autogénéré
    le style est la prochaine étape

--- js/accueil.js
+++ js/accueil.js
@@ -1,25 +1,38 @@
 window.onload = main;
 
 function main(){
+    header(); //cette fonction vient de commun :)
     setSizes();
     sliderInterval = setInterval(slider, 4000);
     document.getElementById("link").onclick= goToSlide;
     //il faut zussi ajiuter un truc qui remet le timer a 0
-    document.getElementById("previous").onclick= function(){if(I>1)I=I-2; slider()};//ca ca bug...
-    document.getElementById("next").onclick= function(){slider()};
+    document.getElementsByClassName("arrows")[0].onclick= function(){if(I>1)I=I-2; slider()};//ca ca bug...
+    document.getElementsByClassName("arrows")[1].onclick= function(){slider()};
     window.onresize = setSizes;
 }
 
 
 function setSizes(){
+    
+    //here we're gonna need a switch, kinda like the css @media...
+    
+    
     var img = document.getElementById("slider").getElementsByTagName("img")[0];
     //et on la met à la bonne hauteur
     img.style.height = window.innerHeight-130+"px";
     //et on la centre.
-    
     img.style.left = (window.innerWidth-img.clientWidth)/2+"px";
     //et remets le footer a la bonne place :
     document.getElementById("slider").style.height = img.clientHeight+4+"px";
+    
+    //on met les fleches à l bonne taille et a la bonne place
+    document.images[2].style.height = (window.innerHeight-130)/5+"px";
+    document.images[2].parentNode.style.top = (img.clientHeight-document.images[2].clientHeight)/2+"px";
+    document.images[2].parentNode.style.left = (window.innerWidth-img.clientWidth)/2+"px";
+    document.images[3].style.height = (window.innerHeight-130)/5+"px";
+    document.images[3].parentNode.style.top = (img.clientHeight-document.images[3].clientHeight)/2+"px";
+    document.images[3].parentNode.style.left = (window.innerWidth-img.clientWidth)/2+img.clientWidth-document.images[3].clientWidth+"px";
+
 }
 
 
@@ -34,7 +47,6 @@ function slider(){
 
     // on place la bonne image dans la division
     document.getElementById("slider").getElementsByTagName("img")[0].src = images[I][1];
-
 
 }
 
--- js/commun.js
+++ js/commun.js
@@ -1,11 +1,4 @@
-//la fonction premiere de ce script est de générer les parties communes de toutes les pages, afin de faciliter la maintenance.
 
-window.onload = main;
-
-function main(){
-    header();
-    
-}
 
 var navMenu = [["Musique", ["beauregard.html", "Beauregard"], ["jazz.html", "Jazz sous les Pommiers"]], ["Cinéma", ["deauville.html", "Festival de deauville"]], ["Tradition", ["medieval.html", "Festival médiéval de Bayeux"], ["cidre.html", "Cidre et Dragons"]]];
 
@@ -16,15 +9,14 @@ function header(){
     //each appending must be in new line. this is crap.
     //this method goes from top to bottom. (kinda...)
     
-    var homeTitle = document.createElement("div");
-    homeTitle.setAttribute("id", "left");
-    var homeLink = document.createElement("a");
-    homeLink.src="#";
-    homeLink.appendChild(document.createElement("img"));
+    dropDown = document.createElement("nav"); //this one needs to be global, it's the menu
+    var firstUl = document.createElement("ul");
+    dropDown.appendChild(firstUl);
+    var clearer = document.createElement("div");
+    clearer.setAttribute("class", "clearer");
+    dropDown.appendChild(clearer);
     
-    //document.createElement("ul")); //can this be done at the end ?
-    var firstUl = document.createElement("ul"); //thanks to this variable ?
-
+    
     for (var element in navMenu){
         var li = document.createElement("li");
         var ul = document.createElement("ul");
@@ -55,8 +47,8 @@ function header(){
     }
     
     
-    document.getElementsByTagName("header")[0].appendChild(firstUl);
-    document.body.appendChild(document.adoptNode(firstUl)); //this works, can be good for putting menu in an other container.
+    document.getElementsByTagName("header")[0].appendChild(dropDown);
+//document.body.appendChild(document.adoptNode(firstUl)); //this works, can be good for putting menu in an other container.
  }
 
 
