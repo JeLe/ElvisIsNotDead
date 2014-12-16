7595c1231ad601f2dbbbb225db96b2727276f08e
JeLe <dev.lecarpentier@gmail.com>
2014-12-16 10:08:08 +0100

    Revert fa644b9..61ce16c
    This rolls back to commit fa644b9b5ba37810a4f25784358edfa724d6c6b0.

--- js/beauregard.js
+++ js/beauregard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:js/beauregard.js
 
 function setSizes(){
     document.getElementById("buttons").style.width=window.innerwidth*0.6+"px";
@@ -71,22 +70,3 @@ function newSetSizes(){
 
 
 }
-=======
-
-function setSizes(){
-    document.getElementById("buttons").style.width=window.innerwidth*0.6+"px";
-    document.getElementsByTagName("iframe")[0].style.width=window.innerwidth*0.6+"px";
-    $(".button1 a").css("font", window.innerHeight*0.07/3+"px/"+window.innerHeight*0.07+"px Junction, sans-serif");
-
-}
-
-//function zwoush(){
-    //scrollTo(0,150);
-}
-function load(){
-    setSizes();
-    //document.getElementsByClassName("button")[1].onclick=zwoush;
-}
-
-    
->>>>>>> FETCH_HEAD:js/commun-event.js
